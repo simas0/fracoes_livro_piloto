afa06d250f3a35666e7330f08976fc7866913942
Augusto <augusto@impa.br>
2016-03-10 17:34:51 -0300

    Adicionando WRAP professor recursivo

--- LPeg/dw2tex.lua
+++ LPeg/dw2tex.lua
@@ -2,10 +2,25 @@ file = io.open (arg[1], "r")
 doc = file:read("*a")
 io.close(file)
 
-inspect = require"inspect"
+--inspect = require"inspect"
+
+function tprint (tbl, indent)
+  if not indent then indent = 0 end
+  for k, v in pairs(tbl) do
+    formatting = string.rep("  ", indent) .. k .. ": "
+    if type(v) == "table" then
+      print(formatting)
+      tprint(v, indent + 1)
+    elseif type(v) == 'boolean' then
+      print(formatting .. tostring(v))
+    else
+      print(formatting .. v)
+    end
+  end
+end
 
 local lpeg = require 'lpeg'
-local R, P, S, C, Cs, Cg, Ct, Cc = lpeg.R, lpeg.P, lpeg.S, lpeg.C, lpeg.Cs, lpeg.Cg, lpeg.Ct, lpeg.Cc
+local R, P, S, C, Cs, Cg, Ct, Cc, V = lpeg.R, lpeg.P, lpeg.S, lpeg.C, lpeg.Cs, lpeg.Cg, lpeg.Ct, lpeg.Cc, lpeg.V
 
 local function token(id, patt) return Ct( Cg(P('') / id, 'tag') * Cg( patt, 'value' ) ) end
 
@@ -17,27 +32,37 @@ end
 
 local digit = R('09')
 local alpha = R('AZ', 'az') + S('áéíóúàèìòùâêÂÊÁÉÍÓÚÀÈÌÒÙüãẽõçÇÃẼÕ')
-local harmless = digit + alpha + S([[():/+-!?.,; ]])
-local symb = S('\\{}$&#^*|_~%=<>\n\t"\'')
-local known = harmless + symb
+local symb = S('():/+-!?.,;\\{}$&#^*|_~%=<>"\' \n\t')
+local known = digit + alpha + symb
 
 -- replacing unknown symbols by a string
-local killunknown = Cs( ( C(known) / '%1' + P(1) / '(símbolo desconhecido)' )^0 )
+local killunknown = Cs( ( C(known) / '%1' + C(P(1)) / '(símbolo desconhecido:%1)' )^0 )
 doc = killunknown:match(doc)
+
+print(doc)
+
+local special = P('**') + P('__') + P([[//]]) + P("''") + P('====') + P('$') + P('<WRAP') + P('</WRAP')
+local harmless = ( digit + alpha + symb ) - special
 
 local simpletext = harmless^1
 local bold = surround('bold', '**', simpletext)
 local under = surround('under', '__', simpletext)
 local italic = surround('italic', [[//]], (harmless - P([[//]]))^1 )
 local mono = surround('mono', "''", simpletext)
+local simplemath = surround('simplemath', '$', simpletext)
 local title = P('=====') * token('title', simpletext) * P('=====')
 
+local W = V'W'
+local wrap = P{
+   W,
+   W = P('<WRAP professor>') * Ct( Cg(P('') / 'wrap', 'tag') * Cg(P('') / 'professor', 'type') * Cg( Ct( ( C(simpletext) + (V'W') )^1 ), 'value' ) ) * P('</WRAP>')
+}
 
+local document = Ct( ( title + bold + under + italic + mono + simplemath + wrap + C(simpletext) + C(known) )^1 )
 
-
-local document = Ct( ( title + bold + under + italic + mono + known )^1 )
-
-print(inspect(document:match(doc), {newline='\n', indent="  "}))
+tprint(document:match(doc))
+--tprint({ a = 1, b = { 4, 5, 6 } })
+--print(inspect(document:match(doc), {newline='\n', indent="  "}))
 
 outstring = ""
 
