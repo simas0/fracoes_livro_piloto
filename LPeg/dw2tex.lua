965a43cd000058c185b0db66b718eb699b16c6e0
Augusto <augusto@impa.br>
2016-06-19 15:09:17 -0300

    Algumas mudancas no parser de dokuwiki

--- LPeg/dw2tex.lua
+++ LPeg/dw2tex.lua
@@ -2,7 +2,11 @@ file = io.open (arg[1], "r")
 doc = file:read("*a")
 io.close(file)
 
---inspect = require"inspect"
+if arg[3] == "aluno" then
+  livrodoaluno = 1
+end
+
+inspect = require"inspect"
 
 function tprint (tbl, indent)
   if not indent then indent = 0 end
@@ -66,9 +70,10 @@ doc = killunknown:match(doc)
 
 local special = P('**') + P('__') + P([[//]]) + P("''") + P('====') + P('$') + P('<WRAP')
    + P('</WRAP') + P('"') + P([[\\]]) + P('{{') + P('}}') + P('/*') + P('*/') + P('<hidden ') + P('</hidden>')
-   + P('\n  *') + P('\n  -') + P('\n    *') + P('\n    -') + P('\n')
+   + P('\n  *') + P('\n  -') + P('\n    *') + P('\n    -') + P('\n') + P(';;#') + P('|')
 local harmless = known - special
 
+local whitespace = P(' ')^0
 local simpletext = harmless^1
 local bold = surround('bold', '**', simpletext)
 local under = surround('under', '__', simpletext)
@@ -77,12 +82,13 @@ local mono = surround('mono', "''", simpletext)
 local newline = token('newline', [[\\]])
 local linefeed = token('linefeed', '\n')
 local simplemath = surround('simplemath', '$', simpletext)
+local atividade = token('atividade', '===== - Atividade =====')
 local title = P('=====') * token('title', simpletext) * P('=====')
 local titlechapter = P('======') * token('title', simpletext) * P('======')
 local titleless = P('====') * token('title', simpletext) * P('====')
 local include = P('{{page>') * token('include', simpletext) * P('}}')
 local image = P('{{') * token('image', simpletext) * P('}}')
-local comment = P('/*') * token('comment', 1 - P('*/'))^0 * P('*/')
+local comment = P('/*') * token('comment', 1 - P('*/'))^0 * P('*/') + P(';;#') * token('comment', 1 - P(';;#'))^0 * P(';;#')
 local quote = surround('quote', '"', Ct( (bold + under + italic + mono + simplemath + token('simple', simpletext))^0 ))
 local decoline = bold + under + italic + mono + quote + simplemath + token('simple', simpletext)
 local item = P('\n  *') * token('item', Ct( decoline^0 ))
@@ -94,10 +100,15 @@ local enumerate = token('enumerate', Ct( enumitem^1 ))
 local doubleenumitem = P('\n    -') * token('doubleenumitem', Ct( decoline^0 ))
 local doubleenumerate = token('doubleenumerate', Ct( doubleenumitem^1 ))
 --local hidden = P('<hidden ') * token('comment', simpletext + bold + under + italic + mono + quote + enumerate + itemize + doubleenumerate + doubleitemize + simplemath
---                                        + newline + linefeed + titlechapter + title + titleless + comment)^0 * P('</hidden>')
+--                                        + newline + linefeed + atividade + titlechapter + title + titleless + comment)^0 * P('</hidden>')
 local hidden = ( P('<hidden ') * (known - P('>'))^0 * P('>') ) + P('</hidden>')
-
-local decotext = bold + under + italic + mono + quote + enumerate + itemize + doubleenumerate + doubleitemize + simplemath + titlechapter
+local cellcontent = token('cellcontent', Ct( (bold + under + italic + mono + quote
+                                                 + simplemath + image + token('simple', simpletext))^1 ) )
+local tabularline = token('tabularline', Ct( ( ( whitespace * P('|') ) * cellcontent )^1
+                                * ( P('|') * whitespace )) * linefeed)
+local tabular = token('tabular', Ct(tabularline^1))
+local decotext = bold + under + italic + mono + quote + enumerate + itemize + doubleenumerate
+   + doubleitemize + simplemath + atividade + titlechapter + tabular
    + title + titleless + include + image + newline + linefeed + comment + hidden + token('simple', simpletext)
 
 local W = V'W'
@@ -116,8 +127,16 @@ local document = Ct( ( decotext + wrap + bighidden + token('error', special) + t
 
 local finalsymb = (P('#') / [[\#]]) + (P('$') / [[\$]]) + (P([[%]]) / [[\%%]]) + (P('&') / [[\&]]) + (P([[\]]) / [[\textbackslash{}]]) + (P('^') / [[\textasciicircum{}]]) + (P('_') / [[\_]]) + (P('{') / [[\{]]) + (P('}') / [[\}]]) + (P('~') / [[\textasciitilde{}]]) + (P('"') / 'QUOTES')
 
+function twothirds(x) return tostring(math.min(6*tonumber(x)/10,600)) end
+
+local formatimagesize = Cs( ( (1 - P('?')) / '' )^1 * ( P('?') / '' )
+      * ( P('direct&') / '' )^-1
+      * ( ( P('0x') / 'height=' ) * (digit^1 / twothirds) * ( P('') / 'pt' ) )^-1
+      * ( ( P('') / 'width=' ) * (digit^1 / twothirds) * ( P('') / 'pt' ) )^-1
+      * ( ( P('x') / ',height=' ) * (digit^1 / twothirds) * ( P('') / 'pt' ) )^-1
+      * ( simpletext / '' )^0 )
 local formatimage = Cs( ( P('') / '/var/www/livro/data/gitrepo/media' ) * ( C(alpha + digit + S('-_.'))
-                    + ( P(' ') / '' ) + ( P(':') / '/' ) )^1 * ( simpletext / '' ) )
+                    + ( P(' ') / '' ) + ( P(':') / '/' ) )^1 * ( simpletext / '' )^0 )
 local formatinclude = Cs( ( P('') / '/var/www/livro/data/gitrepo/pages/' )
       * ( C(alpha + digit + S('-_.')) + ( P(' ') / '' ) + ( P(':') / '/' ) )^1
       * ( P('') / '.txt' ) * ( simpletext / '' )^0 )
@@ -128,12 +147,16 @@ function texprint (tbl, indent)
   if not indent then indent = 0 end
   for k, v in pairs(tbl) do
      local formatting = string.rep("  ", indent)
-     if (v.tag) == "title" then
-        outstr = outstr .. formatting .. '\\section{' .. formatsimple:match(v.value) .. '}\n'
+     if (v.tag) == "atividade" then
+        outstr = outstr .. formatting .. '\\section{Atividade}\n'
+     elseif (v.tag) == "title" then
+        outstr = outstr .. formatting .. '\\section*{' .. formatsimple:match(v.value) .. '}\n'
      elseif (v.tag) == "titleless" then
-        outstr = outstr .. formatting .. '\\subsection{' .. formatsimple:match(v.value) .. '}\n'
+        outstr = outstr .. formatting .. '\\subsection*{' .. formatsimple:match(v.value) .. '}\n'
      elseif (v.tag) == "titlechapter" then
         outstr = outstr .. formatting .. '\\chapter{' .. formatsimple:match(v.value) .. '}\n'
+     elseif (v.tag) == "simplemath" then
+        outstr = outstr .. formatting .. '$' .. v.value .. '$'
      elseif (v.tag) == 'bold' then
         outstr = outstr .. formatting .. '{\\bf ' .. formatsimple:match(v.value) .. '}'
      elseif (v.tag) == 'italic' then
@@ -141,7 +164,7 @@ function texprint (tbl, indent)
      elseif (v.tag) == 'under' then
         outstr = outstr .. formatting .. '{' .. formatsimple:match(v.value) .. '}'
      elseif (v.tag) == 'quote' then
-        outstr = outstr .. formatting .. [[``]] .. texprint(v.value, indent + 1) .. [['']]
+        outstr = outstr .. formatting .. [[``]] .. texprint(v.value, 0) .. [['']]
      elseif (v.tag) == 'newline' then
         outstr = outstr .. formatting .. '\\mbox{} \\newline '
      elseif (v.tag) == 'linefeed' then
@@ -158,17 +181,45 @@ function texprint (tbl, indent)
            outstr = outstr .. formatting .. texprint(document:match(includestring))
         end
      elseif (v.tag) == 'image' then
-        outstr = outstr .. formatting .. '\n\n'
-           .. formatting .. '\\includegraphics[width=\\textwidth, height=4cm, keepaspectratio]{'
-           .. formatimage:match(v.value) .. '}\n\n'
+        if (formatimage:match(v.value)) then
+           local tempsize = 'width=\textwidth,height=4cm'
+           if (formatimagesize:match(v.value)) then
+              tempsize = formatimagesize:match(v.value)
+           end
+           outstr = outstr .. formatting .. ''
+             .. formatting .. '\\includegraphics['
+             .. tempsize .. ', keepaspectratio]{'
+             .. formatimage:match(v.value) .. '}'
+             --.. 'value =<' .. v.value .. '>\n'
+             --.. 'formatsize =<' .. tempsize .. '>\n'
+             --.. 'format =<' .. formatimage:match(v.value) .. '>\n'
+        else
+           outstr = outstr .. formatting .. 'only value =<' .. v.value .. '>\n'
+        end
+     elseif (v.tag) == 'cellcontent' then
+        outstr = outstr .. texprint(v.value, 0)
+     elseif (v.tag) == 'tabularline' then
+        outstr = outstr .. formatting
+        for k, cell in pairs(v.value) do
+           -- print "vvvvvvvvvvvvvvvv"
+           -- print(inspect(cell))
+           -- print "________________"
+           if (k > 1) then outstr = outstr .. '& ' end
+           outstr = outstr .. texprint(cell.value, 0)
+        end
+        outstr = outstr .. '\n'
+     elseif (v.tag) == 'tabular' then
+        outstr = outstr .. formatting .. '\n\\begin{center}\n'.. formatting .. '  \\begin{tabular}{l*{50}{c}}\n'
+           .. texprint(v.value, indent + 2)
+           .. formatting .. '  \\end{tabular}\n'.. formatting .. '\\end{center}\n'
      elseif (v.tag) == 'enumitem' then
         outstr = outstr .. formatting .. '\\item' .. texprint(v.value, indent + 1) .. '\n'
      elseif (v.tag) == 'doubleenumitem' then
         outstr = outstr .. formatting .. '\\item' .. texprint(v.value, indent + 1) .. '\n'
      elseif (v.tag) == 'enumerate' then
-        outstr = outstr .. formatting .. '\n\\begin{enumerate} %s\n' .. texprint(v.value, indent + 1) .. '\\end{enumerate} %s\n'
+        outstr = outstr .. formatting .. '\n\\begin{enumerate} [\\quad a)] %s\n' .. texprint(v.value, indent + 1) .. '\\end{enumerate} %s\n'
      elseif (v.tag) == 'doubleenumerate' then
-        outstr = outstr .. formatting .. '\n\\begin{enumerate} %d\n' .. texprint(v.value, indent + 1) .. '\\end{enumerate} %d\n'
+        outstr = outstr .. formatting .. '\n\\begin{enumerate} [\\quad a)] %d\n' .. texprint(v.value, indent + 1) .. '\\end{enumerate} %d\n'
      elseif (v.tag) == 'item' then
         outstr = outstr .. formatting .. '\\item' .. texprint(v.value, indent + 1) .. '\n'
      elseif (v.tag) == 'doubleitem' then
@@ -180,9 +231,11 @@ function texprint (tbl, indent)
      elseif (v.tag) == 'error' then
         outstr = outstr .. formatting .. '( ERRO:\\{' .. formatsimple:match(v.value) .. '\\} )'
      elseif (v.tag) == 'wrap' then
-        outstr = outstr .. formatting .. '\\begin{' .. v.type .. '}[breakable]{}{}'
-        outstr = outstr .. texprint(v.value, indent + 1)
-        outstr = outstr .. formatting .. '\\end{' .. v.type .. '}'
+        if ((v.type ~= "professor" and v.type ~= "resposta") or not livrodoaluno) then
+            outstr = outstr .. formatting .. '\\begin{' .. v.type .. '*}[breakable]{}{}'
+            outstr = outstr .. texprint(v.value, indent + 1)
+            outstr = outstr .. formatting .. '\\end{' .. v.type .. '*}'
+        end
      end
    end
    return outstr
@@ -204,5 +257,6 @@ outstring = outstring .. texprint(document:match(doc)) .. '\\end{document}'
 file = io.open (arg[2], "w")
 file:write(outstring)
 io.close(file)
+
 
 
